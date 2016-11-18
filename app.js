fb0820582439e9a86bc61179a4bf081296b49416
Muneeb Khan <munibkhan7737@gmail.com>
2016-11-17 23:15:27 -0800

    stop watch chal gaye

--- app.js
+++ app.js
@@ -4,36 +4,29 @@ var mins = 0;
 var hours = 0;
 var a;
 
-function timer(){
-    milisec++;
-    if(milisec === 1000){
+function timer() {
+    document.getElementById("h2").innerHTML = mins + " : " + sec + " : " + milisec++ ;
+    if (milisec == 100) {
         sec++;
         milisec = 0;
-        if(sec === 60) {
-            mins++;
+
+        if (sec == 10) {
             sec = 0;
-            if(mins === 60) {
-                hours++;
-                mins = 0;
-                if(hours === 24){
-                    hours = 0;
-                }
-            }
+            mins++;
         }
     }
-     document.getElementsByTagName("h2").innerHTML = hours + ":" + mins + ":" + sec + ":" + milisec++ ;
 }
 
-function start(){
+function start() {
     a = setInterval(timer, 1);
 }
 
-function stop(){
-clearInterval(a);
+function stop() {
+    clearInterval(a);
 }
 
-function reset(){
+function reset() {
     stop();
-    milisec=sec=mins=hours=0;
-document.getElementsByTagName("h2").innerHTML= "00" + ":" + "00" + ":" + "00";
+    milisec = sec = mins = hours = 0;
+    document.getElementsByTagName("h2").innerHTML = "00" + ":" + "00" + ":" + "00";
 }
